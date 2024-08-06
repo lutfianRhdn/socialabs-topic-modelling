05e66abad58aab07a7e580e97228555fce4ebe09
fajarbh14 <fajarvuana@gmail.com>
2024-08-06 12:21:56 +0700

    Refactor code for better readability and maintainability

--- services/llm.py
+++ services/llm.py
@@ -76,28 +76,36 @@ class Llm:
         # Data dengan bobot kata-kata tertinggi untuk setiap topik
         # Assuming 'words' is a list of (word, weight) tuples
         topic_keywords = words
-        # Create a prompt using the keywords
+                # Create a prompt using the keywords
+                # Create a prompt using the keywords
         role = "AI Linguistik"
         action = "dapat menentukan kalimat dari beberapa topik hasil dari proses topic modeling yang berupa kumpulan kata-kata, "
         step = "dengan mempertimbangkan bobot setiap topik yang ada, dalam merangkai kata-kata kunci menjadi kalimat yang padu untuk sebuah topik yang diperbincangkan di Twitter dengan mengambil kata dari hasil topic modeling lalu menyusunnya menjadi sebuah kalimat yang padu yang mudah dipahami. "
         context = f"Topik ini membahas tentang keyword: {keyword} dengan berbagai pandangan masyarakat terhadap topik tersebut dengan hasil topic modeling dengan 1 topik terdiri dari beberapa kata kunci berikut: {topic_keywords}. "
-        format_str = "dengan format kalimat singkat yang mudah dipahami oleh masyarakat"
+        format_str = f"""dengan format JSON dengan 1 topik untuk 1 kalimat utama Berikut ini adalah format JSON-nya:
+        [
+            {{
+                "kalimat": "..."
+            }}
+        ]
+        ONLY answer in JSON FORMAT without opening words.
+        """
 
-        # Combine components into the RASCEF prompt
+        # Combine components into the prompt
         prompt = f"Anda adalah {role} yang {action} {step} {context}. Buatkan {format_str}"
+
 
         # Generate a completion using the ChatCompletion endpoint
         openai.api_key = openai_key
         openai.api_base = openai_endpoint
         openai.api_type = 'azure'
-        openai.api_version = '2023-05-15'  # this might change in the future
+        openai.api_version = '2023-05-15' # this might change in the future
 
-        deployment_name = openai_deploy_name
-
+        deployment_name= openai_deploy_name 
+        
         try:
             response = openai.ChatCompletion.create(
                 engine=deployment_name,
-                # prompt=start_phrase,
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "system", "content": "You are a helpful assistant."},
@@ -108,13 +116,23 @@ class Llm:
 
             # Extract the generated sentence from the response
             generated_sentence = response['choices'][0]['message']['content'].replace('```', '')
+            pattern = re.compile(r'\[(?:\s*{[^{}]*}\s*,?)*\s*\]')
+            match = pattern.search(generated_sentence)
+
+            if match:
+                json_text = match.group()
+                print(json_text)
+            else:
+                print("Tidak ada JSON yang ditemukan dalam string.")
             
-            # Extract context from the generated sentence
-            context_start = generated_sentence.find("membahas tentang keyword:")
-            context_end = generated_sentence.find(". Buatkan", context_start)
-            context_value = generated_sentence[context_start:context_end].strip() if context_start != -1 and context_end != -1 else generated_sentence
+            # Print the generated sentence
+            res_json = json.loads(json_text)
+            print(type(res_json))
 
+            # Mengambil item pertama dari res_json
+            item = res_json[0]
+            context_value = item['kalimat']
             return context_value
-
+        
         except Exception as e:
             return f"An error occurred: {str(e)}"
